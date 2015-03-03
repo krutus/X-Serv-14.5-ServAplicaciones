28e469dced02ef6caf1aef24957df04478564753
Luis Haro <luisfuenla5@gmail.com>
2015-03-03 11:43:35 +0100

    Subidos los archivos

---
+++ holaapp.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+import webapp
+
+class holaApp(webapp.webApp):
+
+	def process(self,parsedRequest):
+		"""Process the relevant elements of the request.
+
+		Devuelve el codigo HTTP de la respuesta y una pag HTML.
+		"""
+
+		return("200OK", "<html><body><h1>Hola mundo</h1></body></html>")
+
+if __name__=="__main__":
+	testWeb=holaApp("localhost",1234)
